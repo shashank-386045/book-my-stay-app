f7ac80cd49cfcd7c3985522f5e4fea65c0c29040
shashank-386045 <shashank.nuthalapati06@gmail.com>
2026-03-10 23:16:45 +0530

    UC4:Room Search & Availability Check

--- src/bookmystayapp.java
+++ src/bookmystayapp.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-public class bookmystayapp {
+public class bookmystayapp{
 
     public static void main(String[] args) {
 
@@ -8,18 +8,64 @@ public class bookmystayapp {
 
         inventory.addRoomType("Single Room", 5);
         inventory.addRoomType("Double Room", 3);
-        inventory.addRoomType("Suite Room", 2);
+        inventory.addRoomType("Suite Room", 0);
 
-        System.out.println("Hotel Booking System v3.1");
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        Room[] rooms = { single, doubleRoom, suite };
+
+        RoomSearchService searchService = new RoomSearchService();
+
+        System.out.println("Hotel Booking System v4.0");
+        System.out.println("Available Rooms:");
         System.out.println();
 
-        inventory.displayInventory();
+        searchService.searchAvailableRooms(inventory, rooms);
+    }
+}
 
-        inventory.updateAvailability("Single Room", -1);
+abstract class Room {
 
-        System.out.println();
-        System.out.println("Inventory After Update:");
-        inventory.displayInventory();
+    String roomType;
+    int beds;
+    double size;
+    double price;
+
+    Room(String roomType, int beds, double size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+
+    SingleRoom() {
+        super("Single Room", 1, 200, 80);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    DoubleRoom() {
+        super("Double Room", 2, 350, 150);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    SuiteRoom() {
+        super("Suite Room", 3, 600, 300);
     }
 }
 
@@ -38,15 +84,21 @@ class RoomInventory {
     public int getAvailability(String roomType) {
         return inventory.getOrDefault(roomType, 0);
     }
+}
 
-    public void updateAvailability(String roomType, int change) {
-        int current = inventory.getOrDefault(roomType, 0);
-        inventory.put(roomType, current + change);
-    }
+class RoomSearchService {
 
-    public void displayInventory() {
-        for (String roomType : inventory.keySet()) {
-            System.out.println(roomType + " Available: " + inventory.get(roomType));
+    public void searchAvailableRooms(RoomInventory inventory, Room[] rooms) {
+
+        for (Room room : rooms) {
+
+            int available = inventory.getAvailability(room.roomType);
+
+            if (available > 0) {
+                room.displayRoomDetails();
+                System.out.println("Available: " + available);
+                System.out.println();
+            }
         }
     }
 }
