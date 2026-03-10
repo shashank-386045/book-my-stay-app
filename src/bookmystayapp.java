8ba3dcf374f7057e422cbf56e25f894f67450da9
shashank-386045 <shashank.nuthalapati06@gmail.com>
2026-03-10 16:48:00 +0530

    UC1:application entry & welcome message

---
+++ src/bookmystayapp.java
@@ -0,0 +1,11 @@
+public class bookmystayapp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking System");
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: v1.0");
+        System.out.println("Application started successfully.");
+
+    }
+}
